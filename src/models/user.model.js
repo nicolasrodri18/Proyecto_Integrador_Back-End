14cf863382339a96ce838f0844eb089b56eef254
nicolasrodri18 <pufiavila@gmail.com>
2026-03-19 09:54:27 -0500

    Realizacion de las peticiones y controladores del backend

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,7 +1,54 @@
-import db from '../../db.json' assert { type: 'json' }
+import { readFile, writeFile } from 'fs/promises'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
 
-// getAll     - retorna todos los usuarios
-// getById    - retorna un usuario por id
-// create     - crea un nuevo usuario
-// update     - actualiza un usuario por id
-// remove     - elimina un usuario por id
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const DB_PATH = join(__dirname, '../../db.json')
+
+const readDB = async () => {
+  const data = await readFile(DB_PATH, 'utf-8')
+  return JSON.parse(data)
+}
+
+const writeDB = async (data) => {
+  await writeFile(DB_PATH, JSON.stringify(data, null, 2), 'utf-8')
+}
+
+export const getAll = async () => {
+  const db = await readDB()
+  return db.usuarios
+}
+
+export const getById = async (id) => {
+  const db = await readDB()
+  return db.usuarios.find((u) => u.id === id) ?? null
+}
+
+export const create = async (userData) => {
+  const db = await readDB()
+  const newUser = {
+    id: Date.now().toString(),
+    ...userData,
+  }
+  db.usuarios.push(newUser)
+  await writeDB(db)
+  return newUser
+}
+
+export const update = async (id, userData) => {
+  const db = await readDB()
+  const index = db.usuarios.findIndex((u) => u.id === id)
+  if (index === -1) return null
+  db.usuarios[index] = { ...db.usuarios[index], ...userData }
+  await writeDB(db)
+  return db.usuarios[index]
+}
+
+export const remove = async (id) => {
+  const db = await readDB()
+  const index = db.usuarios.findIndex((u) => u.id === id)
+  if (index === -1) return null
+  const [removed] = db.usuarios.splice(index, 1)
+  await writeDB(db)
+  return removed
+}
--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,12 +1,12 @@
 import express from 'express'
-// import { getTasks, getTask, createTask, updateTask, deleteTask } from '../controllers/task.controller.js'
+import { getTasks, getTask, createTask, updateTask, deleteTask } from '../controllers/task.controller.js'
 
 const taskRouter = express.Router()
 
-// taskRouter.get('/', getTasks)
-// taskRouter.get('/:id', getTask)
-// taskRouter.post('/', createTask)
-// taskRouter.put('/:id', updateTask)
-// taskRouter.delete('/:id', deleteTask)
+taskRouter.get('/', getTasks)
+taskRouter.get('/:id', getTask)
+taskRouter.post('/', createTask)
+taskRouter.put('/:id', updateTask)
+taskRouter.delete('/:id', deleteTask)
 
 export default taskRouter
